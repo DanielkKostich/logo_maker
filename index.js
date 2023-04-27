d0b68a896c43405ab69c2ceae68ac501df447160
DanielkKostich <117228151+DanielkKostich@users.noreply.github.com>
2023-04-27 15:48:30 -0500

    got it to write svg files

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'What are your company intials?',
+        name:'initials',
+        default: 'AAA',
+    },
+    {
+        type: 'input',
+        name: 'initials_color',
+        message : 'What color would you like to make the intials?',
+        default : 'black',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Which shape would you like?',
+        choices: ['circle', 'triangle','square'],
+    }, 
+    {
+        type: 'input',
+        name: 'shape_color',
+        message: 'What color do you want to make the shape?',
+        default: 'blue',
+    },
+])
+
+.then((data) => {
+    //const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile('logo.svg', JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
